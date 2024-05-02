a3eaa266ad7275044aa3a3a6e1f7752e2007994c
Vladimir Georgiev <mirovlad@gmail.com>
2024-05-02 10:36:40 +0200

    Ep 05 clean/improve soltuon 06 summation pairs

--- 2023-05-11-05-tuples-and-sets/own_solutions/06_summation_pairs.py
+++ 2023-05-11-05-tuples-and-sets/own_solutions/06_summation_pairs.py
@@ -2,7 +2,7 @@
 nums = map(int, input().split())
 target = int(input())
 
-# Although not explicitly, the description seems to imply the number will never be negative
+# Although not explicitly, the description seems to imply the numbers will never be negative
 # Thus we can right away exclude anything higher than the target
 nums = set(x for x in nums if x <= target)
 
@@ -11,7 +11,7 @@ while nums:
     pair = None
     n1 = nums.pop()
     # To hold the n2 numbers that we try to pair with n1, but didn't give the target
-    used2 = set()
+    reusable_n2 = set()
     while nums:
         n2 = nums.pop()
         if n1 + n2 == target:
@@ -20,10 +20,10 @@ while nums:
             break
         else:
             # n2 didn't pair with n1, so keep n2 for further use
-            used2.add(n2)
+            reusable_n2.add(n2)
 
     # Recover the n2-s that we can still use
-    nums = nums.union(used2)
+    nums = nums.union(reusable_n2)
 
 for pair in pairs:
     print(f"{pair[0]} + {pair[1]} = {target}")
